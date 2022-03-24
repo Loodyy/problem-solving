b24eff4341956b092258a9322879fa83c2961360
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-03-24 13:57:00 +0900

    [Gold V] Title: 토마토, Time: 2716 ms, Memory: 46844 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,45 @@
+import time
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+m, n, h = map(int, input().split())
+
+arr = []
+q = deque()
+for z in range(h):
+    temp = []
+    for y in  range(n):
+        temp.append(list(map(int, input().split())))
+        for x in range(m):
+            if temp[y][x] == 1:
+                q.append((z, y, x)) 
+    arr.append(temp)
+
+d = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
+
+def solve():
+    
+    while q:  
+        a, b, c = q.popleft()
+        for x, y, z in d:
+            z += a
+            y += b
+            x += c
+            if 0 <= x < m and 0 <= y < n and 0 <= z < h and arr[z][y][x] == 0:
+                q.append((z, y, x))
+                arr[z][y][x] = arr[a][b][c] + 1
+
+    cnt = 0
+    for i in arr:
+        for j in i:
+            if 0 in j:
+                return -1
+            cnt = max(cnt, max(j))
+    return cnt - 1  
+
+print(solve())
+    
+    
+
+    
