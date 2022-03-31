28a904e5d78c70ae2be767a14375f6702cfc0405
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-03-31 23:34:45 +0900

    [Gold III] Title: 경사로, Time: 76 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -1,9 +1,49 @@
 def solve(arr):
     global result 
 
+    bool_arr = check(arr)
+    temp = reverse(arr)
+    bool_arr2 = check(temp)
+
+    for i in range(n):
+        if bool_arr[i] and bool_arr2[i]:
+            continue
+        else: result += 1
+
+    temp = rotate()
+
+    bool_arr = check(temp)
+    temp2 = reverse(temp)
+    bool_arr2 = check(temp2)
+
+    for i in range(n):
+        if bool_arr[i] and bool_arr2[i]:
+            continue
+        else: result += 1
+
+# 90도 회전
+def rotate():
+    temp = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            temp[i][j] = arr[j][n-i-1]
+    return temp
+
+# 좌우반전
+def reverse(arr):
+    temp = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n): 
+            temp[i][j] = arr[i][n-1-j]
+    return temp
+    
+# 각 줄마다 좌에서 오른쪽으로만 길이 완성되는지 체크
+def check(arr):
+    global result
+    bool_arr = []
     for i in range(n):
         visited = [False]*n
-        left, right = False, False
+        left = False
         now = arr[i][0]
         for j in range(n):
             if left:
@@ -16,9 +56,8 @@ def solve(arr):
                     if 0 <= j-1-k < n and arr[i][j-1-k] == arr[i][j-1] and visited[j-1-k] == False:
                         visited[j-1-k] = True
                     else:
-                        left = True 
+                        left = True
                         break
-
             elif diff == -1:
                 for k in range(l):
                     if 0 <= j+k < n and  arr[i][j+k] == arr[i][j] and visited[j+k] == False:
@@ -30,52 +69,13 @@ def solve(arr):
                 left = True 
                 break 
             now = arr[i][j]
-        
-        if left:
-            visited = [False]*n
-            now = arr[i][n-1]
-            for j in range(n):
-                if right:
-                    break
-                diff = arr[i][n-1-j] - now
-                if diff == 0:
-                    continue
-                elif diff == 1:
-                    for k in range(l):
-                        if 0 <= n-j+k < n and arr[i][n-j+k] == arr[i][n-j] and visited[n-j+k] == False:
-                            visited[n-j+k] = True
-                        else:
-                            right = True 
-                            break
-                elif diff == -1:
-                    for k in range(l):
-                        if 0 <= n-1-j-k < n and arr[i][n-1-j-k] == arr[n-1-j] and visited[n-1-j-k] == False:
-                            visited[n-1-j-k] = True
-                        else:
-                            right = True
-                            break
-                else: 
-                    right = True
-                    break
-                now = arr[i][n-1-j]
+        bool_arr.append(left)
 
-        if left and right:
-            continue
-        else:
-            result += 1
-
-def rotate():
-    temp = [[0]*n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            temp[i][j] = arr[j][n-i-1]
-    return temp
+    return bool_arr
 
 if __name__ == "__main__":
     n, l = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(n)]
     result = 0
     solve(arr)
-    a = rotate()
-    solve(a)
     print(result)
