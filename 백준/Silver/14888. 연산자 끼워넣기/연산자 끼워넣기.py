54833173006c74af0e31bc0de9e77e5502194f85
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-03-31 18:53:16 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 100 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,61 +1,39 @@
-def solve():
-    comp = []
-    for temp in perm:
-        if comp == temp:
-            continue
-        comp = temp
-        result = nums[0]
-        for i, operators in enumerate(temp):
-            if operators == 0: result += nums[i+1]
-            elif operators == 1: result -= nums[i+1]
-            elif operators == 2: result *= nums[i+1]
-            else: 
-                if result < 0:
-                    result = -result// nums[i+1]
-                    result *= -1
-                else: result = result // nums[i+1]
-        result_num.append(result)
- 
-    return max(result_num), min(result_num)
-
-def permu(cnt):
-    if cnt == len(oper):
-        temp = [x for x in list]
-        perm.append(temp)
+def solve(idx, temp):
+    if idx == n:
+        result.append(temp)
         return
+    else:
+        if op[0] > 0:
+            op[0] -= 1
+            solve(idx+1, temp+nums[idx])
+            op[0] += 1
+        
+        if op[1] > 0:
+            op[1] -= 1
+            solve(idx+1, temp-nums[idx])
+            op[1] += 1
+        
+        if op[2] > 0:
+            op[2] -= 1
+            solve(idx+1, temp*nums[idx])
+            op[2] += 1
 
-    for i, val in enumerate(oper):
-        if visited[i]:
-            continue
-        visited[i] = True
-        list.append(val)
-        permu(cnt+1)
-        list.pop()
-        visited[i] = False
+        if op[3] > 0:
+            op[3] -= 1
+            if temp < 0:
+                temp = -(-temp//nums[idx])
+            else: temp //= nums[idx]
+            solve(idx+1, temp)
+            op[3] += 1
+        return
 
 if __name__ == "__main__":
     n = int(input())
     nums = list(map(int, input().split())) 
     op = list(map(int, input().split())) 
-    oper = []
-    for i in range(4):
-        leng = op[i]
-        if i == 0:
-            for j in range(leng): oper.append(i)
-        elif i == 1:
-            for j in range(leng): oper.append(i)
-        elif i == 2:
-            for j in range(leng): oper.append(i)
-        else:
-            for j in range(leng): oper.append(i)
+    result = []
+    temp = nums[0]
+    solve(1, temp)
 
-    visited = [False] * len(oper)
-    list = []
-    perm = []
-    permu(0)
-
-    result_num = []
-    a, b = solve()
-
-    print(a)
-    print(b)    
+    print(max(result))
+    print(min(result))
