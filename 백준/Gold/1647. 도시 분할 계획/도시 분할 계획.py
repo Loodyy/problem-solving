ac1a1b1518c48be98bdccf0d622e9c37b01cc1aa
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-04-15 18:39:10 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 5776 ms, Memory: 245548 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,40 @@
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+def union_parent(a, b):
+    a, b = find_parent(a), find_parent(b)
+    if a < b: parent[b] = a
+    else: parent[a] = b
+
+def solve():
+    result = 0
+
+    temp = 0
+    for edge in arr:
+        cost, a, b = edge
+        if find_parent(a) != find_parent(b):
+            union_parent(a, b)
+            result += cost
+            temp = cost
+
+    result -= temp    
+
+    print(result)
+    return
+
+if __name__ == "__main__":
+
+    v, e = map(int, input().split())
+    arr = []
+    for _ in range(e):
+        temp = list(map(int, input().split()))
+        arr.append([temp[2], temp[0], temp[1]])
+    arr.sort()
+
+    parent = [0] * (v+1)
+    for i in range(v+1):
+        parent[i] = i
+
+    solve()
