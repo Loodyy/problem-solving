700bc5aa56204cbd566746e2e304b4cf79f5382a
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-04-27 16:59:01 +0900

    [Gold IV] Title: 연구소 3, Time: 1064 ms, Memory: 32528 KB -BaekjoonHub

---
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -0,0 +1,73 @@
+from collections import deque
+from itertools import combinations as cb
+from copy import deepcopy as dc
+INF = int(1e5)
+
+def solve():
+    
+    res = []
+
+    comb = list(cb(candi, m))
+    for com in comb:
+        res.append(bfs(arr, com))
+
+    result = min(res) if min(res) != INF else -1
+    print(result)
+    return
+
+def bfs(temp, birus):
+
+    arr = dc(temp)
+
+    visited = [[False] * n for _ in range(n)]
+    q = deque()
+    for x, y in birus:
+        q.append((x, y))
+        arr[y][x] = 0
+        visited[y][x] = True
+    
+    while q:
+        x, y = q.popleft()
+        for d in dir:
+            tx, ty = x+d[0], y+d[1]
+            if 0 <= tx < n and 0 <= ty < n:
+                if not visited[ty][tx]:
+                    if arr[ty][tx] == 0:
+                        arr[ty][tx] = arr[y][x] + 1
+                        visited[ty][tx] = True
+                        q.append((tx, ty))
+    
+    check = True
+    max_v = 0
+    for x, y in candi:
+        arr[y][x] = -1
+
+    for i, x in enumerate(arr):
+        if not check:
+            break
+        for j, y in enumerate(x):
+            if arr[i][j] == 0:
+                check = False
+                break
+            max_v = max(max_v, y)
+    
+    return max_v if check else INF
+
+if __name__ == "__main__":
+
+    n, m = map(int, input().split())
+    arr = []
+    candi = []
+    for i in range(n):
+        temp = list(map(int, input().split()))
+        for j, x in enumerate(temp):
+            if x == 1:
+                temp[j] = -1 # wall
+            elif x == 2:
+                temp[j] = 0 # birus
+                candi.append((j, i)) # x, y
+        arr.append(temp)
+
+    dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+    solve()
