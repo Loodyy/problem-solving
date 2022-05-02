fec17ac1878f0ac5b8fd898a0175e7fab788537f
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-05-02 15:24:08 +0900

    [Gold IV] Title: 탈출, Time: 96 ms, Memory: 32492 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -2,64 +2,42 @@ from collections import deque
 
 def solve():
 
-    cnt = 1
-    while True:
-        for _ in range(len(q)):
-            x, y = q.popleft()
-            for d in dir:
-                dx, dy = x+d[0], y+d[1]
-                if 0 <= dx < m and 0 <= dy < n:
-                    if not visited_w[dy][dx] and arr[dy][dx] != "X" and (dx, dy) != dst:
-                        visited_w[dy][dx] = True
-                        arr[dy][dx] = "*"
-                        q.append((dx, dy))
-        
-        for _ in range(len(b)):
-            sx, sy = b.popleft()
-            for d in dir:
-                dx, dy = sx+d[0], sy+d[1]
-                if 0 <= dx < m and 0 <= dy < n:
-                    if (dx, dy) == dst:
-                        print(cnt)
-                        return
-                    if arr[dy][dx] != "*" and arr[dy][dx] != "X":
-                        if not visited_b[dy][dx]:
-                            visited_b[dy][dx] = True
-                            b.append((dx, dy))
-        
-        for i in range(n):
-            for j in range(m):
-                if visited_b[i][j] and visited_w[i][j]:
-                    visited_b[i][j] = False
-        
-        check = 0
-        for x in visited_b:
-            check += x.count(True)
-        if not check:
-            print("KAKTUS")
+    while q:
+        x, y = q.popleft()
+        if arr[dst_y][dst_x] == "S": 
+            print(visited[dst_y][dst_x])
             return
-        cnt += 1
+        for d in dir:
+            tx, ty = x+d[0], y+d[1]
+            if 0 <= tx < m and 0 <= ty < n:
+                temp = arr[ty][tx]
+                if arr[y][x] == "S" and (temp == "." or temp == "D"):
+                    arr[ty][tx] = "S"
+                    visited[ty][tx] = visited[y][x] + 1
+                    q.append((tx, ty))
+                elif arr[y][x] == "*" and (temp == "." or temp == "S"):
+                    arr[ty][tx] = "*"
+                    q.append((tx, ty))
+    print("KAKTUS")
+    return
 
 if __name__ == "__main__":
 
     n, m = map(int, input().split())
     arr = []
-    visited_w = [[False] * m for _ in range(n)]
-    visited_b = [[False] * m for _ in range(n)]
-    q, b = deque(), deque()
-    dst = 0
+    visited = [[0] * m for _ in range(n)]
+    q = deque()
+    dst_x, dst_y = 0, 0
     for i in range(n):
         temp = list(input())
         for j, x in enumerate(temp):
-            if x == "D": dst = (j, i)
-            elif x == "S": 
-                visited_b[i][j] = True
-                b.append((j, i))
-            elif x == "*":
-                visited_w[i][j] = True 
-                q.append((j, i))
+            if x == "D": dst_x = j; dst_y = i
+            elif x == "S": q.append((j, i))
         arr.append(temp)
-    
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == "*": q.append((j, i))
+
     dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
     solve()
