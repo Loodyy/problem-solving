e82429a5de34ed9529d4a28abc672309a8a9a54a
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-05-17 20:45:29 +0900

    [Gold V] Title: 저금통, Time: 2552 ms, Memory: 66920 KB -BaekjoonHub

--- 백준/Gold/2421. 저금통/저금통.py
+++ 백준/Gold/2421. 저금통/저금통.py
@@ -1,26 +1,31 @@
-def solve(n):
-
+def solve(n, max_v):
+    prime_c = prime(max_v)
     dp = [[0] * (n+1) for _ in range(n+1)]
     for i in range(1, n+1):
         for j in range(1, n+1):
             temp = 0
-            if prime(int(str(i)+str(j))):
+            if prime_c[int(str(i)+str(j))]:
                 temp = 1
             dp[i][j] = max(dp[i-1][j]+temp, dp[i][j-1]+temp)
             if i == n and j == n:
                 print(dp[i][j]-1)
                 return
 
-def prime(x):
-    if not x or x == 1: return False
-    else:
-        if x == 2: return True
-        for i in range(2, int(x**(1/2)+1)):
-            if not x%i: return False
-        return True
+def prime(max_v):
+
+    prime_c = [True] * (max_v+1)
+    for i in range(2): prime_c[i] = False
+    for i in range(2, max_v+1):
+        mul = 2
+        while i*mul <= max_v: 
+            if prime_c[i*mul]:
+                prime_c[i*mul] = False
+            mul += 1
+
+    return prime_c
 
 if __name__ == "__main__":
 
     n = int(input())
-
-    solve(n) 
+    max_v = int(str(n)+str(n))
+    solve(n, max_v) 
