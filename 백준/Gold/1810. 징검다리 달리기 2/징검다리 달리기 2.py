bdc9f57d7ab75d082928097b58086242c9a7ee35
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-05-26 18:02:58 +0900

    [Gold III] Title: 징검다리 달리기 2, Time: 560 ms, Memory: 124128 KB -BaekjoonHub

--- 백준/Gold/1810. 징검다리 달리기 2/징검다리 달리기 2.py
+++ 백준/Gold/1810. 징검다리 달리기 2/징검다리 달리기 2.py
@@ -1,60 +1,41 @@
+### 37993597번 코드 참조 ### 
+
 import heapq
 INF = int(1e9)
 
-def solve(n, f, arr, graph):
+def solve(n, f, dic):
 
     if f == 0:
         return 0
 
-    distance = [INF] * (n+1)
     q = []
-    heapq.heappush(q, (0, 0))
-    distance[0] = 0
+    heapq.heappush(q, (0, 0, 0)) # cost, x, y
+    dic[(0, 0)] = 0
 
     result = []
 
     while q:
-        dist, now = heapq.heappop(q)
-        if dist <= distance[now]:
-            for v in graph[now]:
-                next, cost = v[0], v[1]+dist
-                if cost < distance[next]:
-                    distance[next] = cost
-                    if arr[next][1] == f:
-                        result.append(round(cost))
-                    heapq.heappush(q, (cost, next))
+        dist, x, y = heapq.heappop(q)
+        for i in range(-2, 3):
+            for j in range(-2, 3):
+                tx, ty = x+j, y+i
+                if dic.get((tx, ty)):
+                    d = ((x-tx)**2 + (y-ty)**2)**(1/2)
+                    if dist+d < dic[(tx, ty)]:
+                        dic[(tx, ty)] = dist+d
+                        heapq.heappush(q, (dist+d, tx, ty))
+                        if ty == f: result.append(round(dist+d))
 
     if len(result):
         return min(result)
     else: return -1
 
-def check(x, y, nx, ny):
-    if abs(x-nx) <= 2 and abs(y-ny) <= 2:
-        dist = ((x-nx)**2 + (y-ny)**2)**(1/2)
-        return dist
-    else:
-        return 0
-
 if __name__ == "__main__":
 
     n, f = map(int, input().split())
-    arr = [(0, 0)]
+    dic = {}
     for i in range(n):
         a, b = map(int, input().split())
-        arr.append((a, b))
+        dic[(a, b)] = INF
 
-    arr.sort()
-    
-    graph = [[] for _ in range(n+1)]
-    for i, a in enumerate(arr):
-        ax, ay = a
-        for j in range(i, n+1):
-            bx, by = arr[j]
-            if bx - ax > 2:
-                break
-            d = check(ax, ay, bx, by)
-            if d:
-                graph[i].append((j, d))
-                graph[j].append((i, d))
-
-    print(solve(n, f, arr, graph))
+    print(solve(n, f, dic))
