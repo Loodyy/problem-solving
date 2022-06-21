64f3b8185fea0aa0e2618372511cc7a3d2726ca5
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-06-21 13:09:37 +0900

    [Bronze II] Title: 분산처리, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,18 +1,11 @@
+import sys
+input = sys.stdin.readline
+
 k = int(input())
 
 for _ in range(k):
     a, b = map(int, input().split())
-    now = a%10
-    loop = [now]
-    for i in range(1, b+1): 
-        now = (now*a)%10
-        if now == a:
-            break
-        loop.append(now)
-
+    num = pow(a, b, 10)
     
-    idx = b % len(loop)
-    res = loop[idx-1]
-    if res == 0:
-        res = 10
+    res = num if num != 0 else 10
     print(res)
