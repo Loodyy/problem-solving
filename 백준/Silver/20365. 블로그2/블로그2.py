01f15c71e4deb143003ae1929fc8d5efcd3aafe0
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-06-22 23:35:54 +0900

    [Silver II] Title: 블로그2, Time: 180 ms, Memory: 31328 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.py
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -1,7 +1,8 @@
 n = int(input())
 string = input()
-comp = string[0]
+
 dic = {"B": 0, "R": 0}
+comp = string[0]
 dic[comp] += 1
 for i in range(1, n):
     now = string[i]
@@ -9,9 +10,4 @@ for i in range(1, n):
         dic[now] += 1
         comp = now
 
-if dic["B"] > dic["R"]:
-    res = 1+dic["R"]
-else:
-    res = 1+dic["B"]
-
-print(res)
+print(min(dic["B"], dic["R"])+1)
