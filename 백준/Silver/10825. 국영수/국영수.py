836a7e40317be8ce29b2946fcff160b743d41634
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-06-23 10:31:51 +0900

    [Silver IV] Title: 국영수, Time: 568 ms, Memory: 152192 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+for _ in range(n): 
+    name, a, b, c = input().split()
+    arr.append([name, int(a), int(b), int(c)])
+
+arr.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for x in arr:
+    print(x[0])
