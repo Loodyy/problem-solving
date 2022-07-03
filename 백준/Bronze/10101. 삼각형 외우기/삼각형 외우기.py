beab4cb0e41299dd61326a3edd62a6f5892daee5
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-07-03 23:42:13 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,10 @@
+arr = [int(input()) for _ in range(3)]
+if sum(arr) == 180:
+    if arr == [60, 60, 60]:
+        print('Equilateral')
+    elif arr[0] == arr[1] or arr[1] == arr[2] or arr[0] == arr[2]:
+        print('Isosceles')
+    else:
+        print('Scalene')
+else:
+    print("Error")
