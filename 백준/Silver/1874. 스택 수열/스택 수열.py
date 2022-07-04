52475bd0427547e1453371ece5cf300649ef392a
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-07-04 23:11:17 +0900

    [Silver II] Title: 스택 수열, Time: 4036 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,28 +1,22 @@
 def solve():
-    cnt, tidx, now = 0, 0, 1
-    
-    while cnt < n*2:
-        tar = arr[tidx]
-        if st and st[-1] == tar:
+    now = 1
+    for _ in range(n):
+        tar = int(input())
+        while now <= tar:
+            st.append(now)
+            res.append("+")
+            now += 1
+        if st[-1] == tar:
             st.pop()
-            tidx += 1
-            cnt += 1
             res.append("-")
-            continue
-        st.append(now)
-        now += 1
-        cnt += 1
-        res.append("+")
-
-        if now > n+1:
+        else:
             print("NO")
-            return
-
+            return 
+    
     for elem in res:
         print(elem)
 
 n = int(input())
-arr = [int(input()) for _ in range(n)]
 st, res = [], []
 
 solve()
