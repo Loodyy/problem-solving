e0641aa0b75cb74fe4258b6b2984165bb42bc5b7
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-07-06 22:34:50 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,16 +1,8 @@
 n, k = map(int, input().split())
 arr = list(range(1, n+1))
-idx, cnt, total = -1, 0, 0
-res = []
-while total < n:
-    if arr[idx%n] == 0:
-        idx += 1
-        continue
-    if cnt == k:
-        res.append(str(arr[idx%n]))
-        total += 1
-        arr[idx%n], cnt = 0, 0
-    idx += 1
-    cnt += 1
+res, idx = [], 0
+while arr:
+    idx = (idx+k-1)%len(arr)
+    res.append(str(arr.pop(idx)))
 
 print("<"+", ".join(res)+">")
