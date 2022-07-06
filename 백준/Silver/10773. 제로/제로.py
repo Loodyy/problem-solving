fe8de73cbf2b9e7feec3c713363d6b1a07b28e38
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-07-06 18:58:02 +0900

    [Silver IV] Title: 제로, Time: 132 ms, Memory: 118320 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+K = int(input())
+
+arr = []
+for i in range(K):
+    num = int(input())
+    if num != 0:
+        arr.append(num)
+    else:
+        arr.pop()
+
+print(sum(arr))
