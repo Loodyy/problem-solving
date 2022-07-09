64b164d75c4fe723ac7251519ede7218389468bd
AJM <74178065+AnJeongMin@users.noreply.github.com>
2022-07-09 20:06:34 +0900

    [Silver I] Title: 회의실 배정, Time: 472 ms, Memory: 131220 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,24 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-def select(start, i):
-    for idx in range(i, len(meet)):
-        m = meet[idx]
-        if m[0] < start:
-            continue
-        return idx
-    return -1
-
 n = int(input())
 meet = [list(map(int, input().split())) for _ in range(n)]
 meet.sort(key=lambda x: (x[1], x[0]))
 
-cnt, idx, start = 0, -1, 0
-while True:
-    idx = select(start, idx+1)
-    if idx == -1:
-        break
-    start = meet[idx][1]
-    cnt += 1
+cnt, start = 0, 0
+for i in range(n):
+    if start <= meet[i][0]:
+        cnt += 1
+        start = meet[i][1]
 
 print(cnt)
