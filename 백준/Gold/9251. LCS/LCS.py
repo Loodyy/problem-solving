5423f5a5ffd19a036bb4b042f12cd0441ad99021
AJM <74178065+AnJeongMin@users.noreply.github.com>
2023-01-18 16:14:28 +0900

    [Gold V] Title: LCS, Time: 352 ms, Memory: 55676 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,18 +1,19 @@
-def solve():
+def main() -> None:
+    a, b = input(), input()
+    maxLen = solve(a, b)
+    print(maxLen)
 
-    a, b = len(x), len(y)
-    dp = [[0] * (b+1) for _ in range(a+1)]
-    for i in range(1, a+1):
-        for j in range(1, b+1):
-            if x[i-1] == y[j-1]: dp[i][j] = dp[i-1][j-1] + 1
-            else: dp[i][j] = max(dp[i][j-1], dp[i-1][j]) 
+def solve(a, b) -> int:
+    aLen, bLen = len(a), len(b)
+    dp = [[0] * (bLen + 1) for _ in range(aLen + 1)]
+    for i in range(1, aLen + 1):
+        for j in range(1, bLen + 1):
+            aChar, bChar = a[i - 1], b[j - 1]
+            if aChar == bChar:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
     
-    print(dp[a][b])
-    return
+    return dp[-1][-1]
 
-if __name__ == "__main__":
-
-    x = input()
-    y = input()
-
-    solve()
+main()
