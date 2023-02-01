819c6db63d354df92ca2e7c993ff1dafcb6293df
AJM <74178065+AnJeongMin@users.noreply.github.com>
2023-02-01 19:07:01 +0900

    [Silver I] Title: IOIOI, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,16 +1,11 @@
 n, m = int(input()), int(input())
+
 targ = input()
+comp = "I" + "OI"*n
 
-res = cnt = idx = 0
-while idx < m:
-    if targ[idx:idx+3] == "IOI":
-        idx += 2
+cnt = 0
+for i in range(m):
+    if comp == targ[i:i+len(comp)]:
         cnt += 1
-        if cnt == n:
-            res += 1
-            cnt -= 1
-    else:
-        idx += 1
-        cnt = 0
 
-print(res)
+print(cnt)
