6b7cb713d4c276c3c9d7ce565369f6c643c15809
AJM <74178065+AnJeongMin@users.noreply.github.com>
2023-02-01 19:33:36 +0900

    [Silver I] Title: IOIOI, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,25 +1,11 @@
-def main():
-    n = int(input())
-    maxLen = int(input())
-    target = input()
-    cnt = solve(n, maxLen, target)
-    print(cnt)
+n, m = int(input()), int(input())
 
-def solve(n, maxLen, target) -> int:
-    cnt = 0
-    comp: str = "I" + "OI" * n
-    for i in range(maxLen):
-        if i + len(comp) > maxLen:
-            break
-            
-        isMatch = True
-        for j, x in enumerate(comp):                
-            if i + j < maxLen and x != target[i + j]:
-                isMatch = False
-                break
+targ = input()
+comp = "I" + "OI"*n
 
-        cnt += bool(isMatch)
-            
-    return cnt
+cnt = 0
+for i in range(m):
+    if comp == targ[i:i+len(comp)]:
+        cnt += 1
 
-main() 
+print(cnt)
