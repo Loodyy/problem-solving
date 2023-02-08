ec80658886d54c58a1d3f658258c5934d912d516
AJM <74178065+AnJeongMin@users.noreply.github.com>
2023-02-08 16:30:49 +0900

    [Gold III] Title: 아기 상어, Time: 136 ms, Memory: 34280 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,93 +1,69 @@
 from collections import deque
 
-def solve():
-    cnt = 0
-    while True:
-        idxs = check(shark[2])
-         
-        q = deque()
-        visited = [[n*n for _ in range(n)] for _ in range(n)]
-        a, b = shark[0], shark[1]
+def checkClosestFish(shark, board, fishs: dict):
+    sx, sy, sw = shark
+    n = len(board)
+    catched = []
+
+    visited = [[0] * n for _ in range(n)]
+    dq = deque([(sx, sy)])
+    visited[sy][sx] = -1
+    while dq:
+        x, y = dq.popleft()
+        for dx, dy in dir:
+            tx, ty = x + dx, y + dy
+            if not isRange(tx, ty, len(board)) or visited[ty][tx] != 0:
+                continue
+            fishWeight = fishs[(tx, ty)] if (tx, ty) in fishs else 0
+            if fishWeight > sw:
+                continue
+            elif fishWeight == sw or fishWeight == 0:
+                dq.append((tx, ty))
+                visited[ty][tx] = visited[y][x] + 1
+            else:
+                catched.append((tx, ty, visited[y][x] + 1))
         
-        visited[b][a] = 0
-        q.append([a, b])
-        while q:
-            x, y = q.pop()
-            for d in dir:
-                dx, dy = d[0], d[1]
-                if 0 <= x+dx < n and 0 <= y+dy < n and arr[y+dy][x+dx] <= shark[2]:
-                    if visited[y+dy][x+dx] <= visited[y][x] + 1:
-                        continue
-                    visited[y+dy][x+dx] = visited[y][x] + 1
-                    q.append([x+dx, y+dy])
-
-        bool = False
-        for idx in idxs:
-            if visited[fish[idx][2]][fish[idx][1]] != n*n:
-                bool = True
-        if not bool:
-            print(cnt)
-            return
-
-        dist = n*n 
-        midx = 0
-        for idx in idxs:
-            now = fish[idx]
-            if visited[now[2]][now[1]] < dist:
-                dist = visited[now[2]][now[1]]
-                midx = idx
-        
-        mins = [] # 가까운 거리 중복
-        mx, my = fish[midx][1], fish[midx][2]
-        for idx in idxs:
-            if visited[fish[idx][2]][fish[idx][1]] == dist:
-                mins.append(idx)
-        
-        if len(mins) > 1:
-            for min in mins:
-                if my > fish[min][2]:
-                    midx = min
-                    mx, my = fish[min][1], fish[min][2]
-                elif my == fish[min][2]:
-                    if mx > fish[min][1]:
-                        midx = min
-                        mx, my = fish[min][1], fish[min][2]
-
-        tx, ty = fish[midx][1], fish[midx][2]
-        fish.pop(midx)
-        arr[ty][tx] = 9
-        arr[shark[1]][shark[0]] = 0
-        shark[0], shark[1] = tx, ty
-        cnt += dist
-        shark[3] += 1
-        if shark[2] == shark[3]:
-            shark[2] += 1
-            shark[3] = 0
-        
-
-def check(shark_w):
-    idxs = []
-
-    for i, x in enumerate(fish):
-        if shark_w > x[0]:
-            idxs.append(i)
-
-    return idxs
-
-if __name__ == "__main__":
-    n = int(input())
-    fish = []
-    shark = []
-    arr = []
-    for i in range(n):
-        temp = list(map(int, input().split()))
-        for j in range(n):
-            if 0 < temp[j] < 7:
-                fish.append([temp[j], j, i])
-            elif temp[j] == 9:
-                shark = [j, i, 2, 0] # x, y, weigth, eat
-        arr.append(temp)
+    if len(catched) == 0:
+        return -1
     
-    dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+    res = sorted(catched, key = lambda x: (x[2], x[1], x[0]))
+    return res[0]
+            
+def isRange(x, y, n):
+    return 0 <= x < n and 0 <= y < n
+        
+dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+n = int(input())
+shark = [0, 0, 0]
+eatCnt = 0
+fishs = {}
+board = []
+for y in range(n):
+    temp = list(map(int, input().split()))
+    for x in range(n):
+        curr = temp[x]
+        if curr == 9:
+            shark = [x, y, 2]
+        else:
+            fishs[(x, y)] = curr
+    board.append(temp)
+board[shark[1]][shark[0]] = 0
 
-    solve()
+time = 0
+while True:
+    res = checkClosestFish(shark, board, fishs)
+    if res == -1:
+        break
+    sx, sy, sw = shark
+    nx, ny, t = res
+    board[ny][nx] = 0
+    shark[0], shark[1] = nx, ny
+    fishs.pop((nx, ny))
+    time += t + 1
+    
+    eatCnt += 1
+    if eatCnt == sw:
+        eatCnt = 0
+        shark[2] += 1
+
+print(time)
