590f54c6c6de0fcb950e3f26be2551eb29bd84b7
Jeongmin An <74178065+loody7@users.noreply.github.com>
2024-03-29 20:52:53 +0900

    [Gold V] Title: 수 나누기 게임, Time: 440 ms, Memory: 143348 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -2,23 +2,19 @@ n = int(input())
 arr = list(map(int, input().split()))
 ans = [0] * n
 
-num_set = dict()
+num_map = dict()
+for i, num in enumerate(arr):
+    num_map[num] = i
+
 max_num = max(arr)
-for i in range(n):
-    cur = arr[i]
+for i, num in enumerate(arr):
     mul = 1
+    cur = num
     while cur <= max_num:
-        num_set.setdefault(cur, []).append(i)
+        if cur in num_map and not num_map[cur] == i:
+            ans[i] += 1
+            ans[num_map[cur]] -= 1
         mul += 1
-        cur = arr[i] * mul
-        
-for i in range(n):
-    cur = arr[i]
-    if cur in num_set:
-        for j in num_set[cur]:
-            if i == j:
-                continue
-            ans[i] -= 1
-            ans[j] += 1
+        cur = num * mul 
 
 print(*ans)
