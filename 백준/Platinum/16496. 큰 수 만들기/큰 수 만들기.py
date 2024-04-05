6d05335d425feff7773000baba815ecaacf0a3cc
Jeongmin An <ajhappy12@gmail.com>
2024-04-05 18:09:34 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -1,12 +1,7 @@
-from functools import cmp_to_key
-
 N = int(input())
 A = list(input().split())
 
-def key_func(x, y):
-    return int(x+y) - int(y+x)
-
-A.sort(key=cmp_to_key(key_func), reverse=True)
+A.sort(key=lambda x: x * 9, reverse=True)
 
 ans = "".join(A)
 print(ans if int(ans) != 0 else 0)
