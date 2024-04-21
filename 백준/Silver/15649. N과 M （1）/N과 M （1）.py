2cc559677ac2228ae82f9e271d010d68c73f486c
Jeongmin An <ajhappy12@gmail.com>
2024-04-21 16:51:43 +0900

    [Silver III] Title: N과 M (1), Time: 128 ms, Memory: 34772 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,20 @@
 N, M = map(int, input().split())
 ans = []
 
-def bfs(curr, num_set: set):
+def bfs(curr, used):
     if len(curr) == M:
         ans.append(" ".join(map(str, curr)))
         return
     
     for i in range(1, N+1):
-        if i in num_set:
+        if used[i]:
             continue
-        next_set = num_set.copy()
-        next_set.add(i)
-        curr.append(i)
-        bfs(curr, next_set)
-        curr.pop()
 
-bfs([], set())
-[print(a) for a in ans]
+        used[i] = True
+        curr.append(i)
+        bfs(curr, used)
+        curr.pop()
+        used[i] = False
+
+bfs([], [False] * (N+1))
+print("\n".join(ans))
